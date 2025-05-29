301f0fcdb50e92a7643463b2fe004fef0f5b8454
Mangloo <sudhirtemp123@gmail.com>
2025-05-29 13:32:58 +0530

    fixes

--- Backend/app.js
+++ Backend/app.js
@@ -22,7 +22,12 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://coincrest1.netlify.app",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(
   "/uploads/profilePics",
