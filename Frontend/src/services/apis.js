399d5487dc4933801cf11642348812f22cfae5d2
Mangloo <sudhirtemp123@gmail.com>
2025-05-29 12:28:06 +0530

    fixes

--- Frontend/src/services/apis.js
+++ Frontend/src/services/apis.js
@@ -1,5 +1,7 @@
 const BASE_URL = `${import.meta.env.VITE_BACKEND_URL}/api/v1`;
 
+console.log(BASE_URL);
+
 // AUTH ENDPOINTS
 export const authEndpoints = {
   SIGNUP_API: BASE_URL + "/auth/signup",
