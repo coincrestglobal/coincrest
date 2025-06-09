c94fadcbdc6b590ccc681e4cb7bef67d718c22f3
coincrestglobal <consumercoincrest@gmail.com>
2025-06-09 12:47:08 +0530

    Update constants.js

--- Backend/config/constants.js
+++ Backend/config/constants.js
@@ -1,13 +1,13 @@
-const MIN_WITHDRAWAL_AMOUNT = 1;
+const MIN_WITHDRAWAL_AMOUNT = 30;
 
 // Minimum number of days between two withdrawals (if applicable elsewhere)
-const MIN_WITHDRAWAL_INTERVAL_DAYS = 0;
+const MIN_WITHDRAWAL_INTERVAL_DAYS = 7;
 
 // Minimum number of days required between investment and redemption
-const MIN_REDEMPTION_INTERVAL_DAYS = 0;
+const MIN_REDEMPTION_INTERVAL_DAYS = 7;
 
 // 💰 Minimum deposit amount required to qualify as "valid referral"
-const MIN_VALID_REFERRAL_DEPOSIT = 1;
+const MIN_VALID_REFERRAL_DEPOSIT = 100;
 
 module.exports = {
   MIN_WITHDRAWAL_AMOUNT,
